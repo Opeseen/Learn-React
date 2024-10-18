0fd1ffcfce56520b8bcbe59d376dc1c359b90edd
Opeseen <Opeyemi5055@yahoo.com>
2024-10-18 06:01:42 +0100

    Deployed

--- Dipesh_React_Course/contact-api/src/components/ContactList.js
+++ Dipesh_React_Course/contact-api/src/components/ContactList.js
@@ -1,22 +1,15 @@
 import React from "react";
+import CardContact from "./ContactCard";
 
 const ContactList = (props) => {
   const renderContactList = props.contact.map((contact) =>{
     return(
-      <div className="item">
-        <div className="content">
-          <div className="header">{contact.name}</div>
-          <div>{contact.email}</div>
-            <div>
-             <i className="trash alternate outline icon"></i> 
-            </div>
-        </div>
-      </div>
+      <CardContact con={contact} />
     )
   });
-    return <div className="ui celled list">
-      {renderContactList}
-    </div>
+  return <div className="ui celled list">
+    {renderContactList}
+  </div>
 
 };
 
