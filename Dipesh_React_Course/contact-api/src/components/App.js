262185e7bfb00c3b73472fd6b0762eee3b60b9a8
Opeseen <opeyemi5055@yahoo.com>
2024-10-28 17:02:15 +0100

    Deployed

--- Dipesh_React_Course/contact-api/src/components/App.js
+++ Dipesh_React_Course/contact-api/src/components/App.js
@@ -1,15 +1,25 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css'
 import Header from './Header';
 import {AddContact} from './AddContact';
 import ContactList from './ContactList';
 
 function App() {
-  const [contacts, setContacts] = useState([])
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([]);
 
   const contactHandler = (input) => {
     setContacts([...contacts, input]);
   };
+
+  useEffect(() => {
+    const retrieveContact = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (retrieveContact) setContacts(retrieveContact);
+  }, []);
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+  
   return (
     <div className='ui container'>
       <Header />
